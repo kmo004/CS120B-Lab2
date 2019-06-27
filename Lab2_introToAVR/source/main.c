59b8ec4e1095cfabd81f3ad80f545048a29197b7
kmo004 <kmo004@ucr.edu>
2019-06-26 19:55:52 -0700

    ex2 update

--- Lab2_introToAVR/source/main.c
+++ Lab2_introToAVR/source/main.c
@@ -11,54 +11,27 @@
  #ifdef _SIMULATE_
  #include "simAVRHeader.h"
  #endif
-/*
-int main(void){
-DDRA = 0x00; PORTA = 0xFF;
-DDRB = 0xFF; PORTB = 0x00;
-   unsigned char P0 = 0x00;
-   unsigned char P1 = 0x00;
-   
-    while (1) {
-	
-		P0 = PINA & 0x01;
-		P1 = PINA & 0x02;
-		
-		
-		if( P0 == 0x01 && P1 == 0x00){
-			PORTB = 0x01;
-		}
-		else{
-			PORTB = 0x00;
-			}
-	}
-return 0;
-}
-*/
 
 int main(void){
 	DDRA = 0x00; PORTA = 0xFF;
 	DDRC = 0xFF; PORTC = 0x00;
 
 	unsigned char cntavail = 0x00;
-	unsigned char parkSpace= 0x00;
 
 	while(1){
-		
-		parkSpace = PINA;
-
-		if((parkSpace & 0x01)){
-			++cntavail;
+		if(PINA & 0x01) {
+			cntavail++;
+		} 
+		if(PINA & 0x02) {
+			cntavail++;
 		}
-		if((parkSpace & 0x02)){
-			++cntavail;
+		if(PINA & 0x04) {
+			cntavail++;
 		}
-		if((parkSpace & 0x04)){
-			++cntavail;
+		if(PINA & 0x08) {
+			cntavail++;
 		}
-		if((parkSpace & 0x08)){
-			++cntavail;
-		}
-		PORTC = 0x04 - cntavail;
+		PORTC = 4 - cntavail;
 		cntavail = 0;
 	}
 	return 0;
