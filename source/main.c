499d6e8b0082e218c3b194c7f0cfa37b6a48a068
kmo004 <kmo004@ucr.edu>
2019-06-27 12:02:04 -0700

    removed accidental line that made it stopped working

---
+++ source/main.c
@@ -0,0 +1,50 @@
+/*	Author: kmo004
+ *  Partner(s) Name: Moker(Ke) Bellomo
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+ #include <avr/io.h>
+ #ifdef _SIMULATE_
+ #include "simAVRHeader.h"
+ #endif
+
+int main(void) {
+    DDRA = 0x00; PORTA = 0xFF;
+    DDRC = 0xFF; PORTC = 0x00;
+	
+    unsigned char cntavail = 0x00;
+	
+    while (1) {
+		if(PINA & 0x01) {
+		       cntavail = cntavail + 1;
+		} 
+		if(PINA & 0x02) {
+		  cntavail = cntavail + 1;
+		}
+		if(PINA & 0x04) {
+		       cntavail = cntavail + 1;
+		}
+		if(PINA & 0x08) {
+		       cntavail = cntavail + 1;
+		}
+		
+		cntavail = 4 - cntavail;
+		
+	    
+	    	if(cntavail == 0x00) {
+			PORTC = 0x80;
+		}
+	    	else{
+			PORTC = cntavail;
+		}
+		
+
+		cntavail = 0;
+    }
+	
+return 1;
+}
